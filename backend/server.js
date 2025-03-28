aaf8f13d925cb1d0b90d3d5c85daff367ffc3ffb
Angelica091 <adpablete@paterostechnologicalcollege.edu.ph>
2025-03-28 20:36:37 +0800

    added some file

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const studentRoutes = require("./routes/students");
+
+
 const app = express();
 
 
